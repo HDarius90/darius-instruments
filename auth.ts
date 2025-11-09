acabe765ca665da2d0d2968aaf311e4a168be4bd
Hadjati Darius <72970048+HDarius90@users.noreply.github.com>
2025-11-09 14:49:50 +0100

    Middleware hitting Vercel hobby tier limits fix

--- auth.ts
+++ auth.ts
@@ -1,13 +1,12 @@
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth, { NextAuthConfig } from 'next-auth';
 import { prisma } from '@/db/prisma';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compareSync } from 'bcrypt-ts-edge';
-import type { NextAuthConfig } from 'next-auth';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { authConfig } from './auth.config';
 
-export const config: NextAuthConfig = {
+export const config = {
   pages: {
     signIn: '/sign-in',
     error: '/sign-in',
@@ -16,7 +15,6 @@ export const config: NextAuthConfig = {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       credentials: {
@@ -56,6 +54,7 @@ export const config: NextAuthConfig = {
     }),
   ],
   callbacks: {
+    ...authConfig.callbacks,
     async session({ session, user, trigger, token }: any) {
       // Ser the user ID from the token
       session.user.id = token.sub;
@@ -86,31 +85,7 @@ export const config: NextAuthConfig = {
       }
       return token;
     },
-    authorized({ request, auth }: any) {
-      // Check for session cart cookie
-      if (!request.cookies.get('sessionCartId')) {
-        // Generate new session cart id cookie
-        const sessionCartId = crypto.randomUUID();
-
-        // Clone the req headers
-        const newRequestHeaders = new Headers(request.headers);
-
-        // Create new response and add the new headers
-        const response = NextResponse.next({
-          request: {
-            headers: newRequestHeaders,
-          },
-        });
-
-        // Set newly generated sessionCartId in the response cookies
-        response.cookies.set('sessionCartId', sessionCartId);
-
-        return response;
-      } else {
-        return true;
-      }
-    },
   },
-};
+} as NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
